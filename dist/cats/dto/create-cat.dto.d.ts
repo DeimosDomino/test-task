eb757f61d99aca6740e831c5441ca027ba2bfd53
DeimosDomino <Deimo.s@yandex.ru>
2022-01-24 20:21:52 +0300

    added .gitignore

--- dist/cats/dto/create-cat.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateCatDto {
-    readonly name: string;
-    readonly color: string;
-    readonly breed: string;
-    readonly age: number;
-    readonly imgLink: string;
-    readonly price: number;
-    readonly isBooked: boolean;
-}
