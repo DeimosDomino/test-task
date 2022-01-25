3a453f3afcceeadc2d8050803378a471589427ee
DeimosDomino <Deimo.s@yandex.ru>
2022-01-25 10:29:17 +0300

    added process.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,24 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {CatEntity} from "./cats/entities/Cat.entity";
 
 @Module({
-  imports: [CatsModule, TypeOrmModule.forRoot()],
+  imports: [CatsModule, TypeOrmModule.forRoot({
+    type: "postgres",
+    host: process.env.POSTGRES_HOST || "localhost",
+    port: parseInt(process.env.POSTGRES_PORT) ||  5432,
+    username: process.env.POSTGRES_USER || "deimos",
+    password: process.env.POSTGRES_PASSWORD || "123",
+    database: process.env.POSTGRES_DB || "deimos",
+    synchronize: false,
+    entities: [
+      "dist/**/*.entity{.ts,.js}"
+    ],
+    migrations: [
+      "src/migrations/*.ts"
+    ],
+    cli: {
+      "migrationsDir": "migrations"
+    }
+  })],
   controllers: [],
   providers: [],
 })
